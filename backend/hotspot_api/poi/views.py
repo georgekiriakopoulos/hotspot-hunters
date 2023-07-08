b025d562ebaaee26ef8323a288f6a7f96d111fd0
giorgos_kiriakopoulos <gkiriakopoulos@thehomli.com>
2023-07-08 20:11:19 +0300

    feat: add Categories, add fields district/title/description to POI model

--- backend/hotspot_api/poi/views.py
+++ backend/hotspot_api/poi/views.py
@@ -3,7 +3,7 @@ import pandas as pd
 from rest_framework.response import Response
 from .serializers import PoiUploadSerializer
 from .serializers import PointOfInterestSerializer
-from .models import PointOfInterest
+from .models import PointOfInterest, Category
 from rest_framework import status
 from rest_framework.views import APIView
 
@@ -17,11 +17,15 @@ class UploadPoiView(generics.CreateAPIView):
         file = serializer.validated_data['file']
         reader = pd.read_csv(file, encoding_errors='ignore')
         for _, row in reader.iterrows():   
-            print(row)
+            category = Category.objects.get_or_create(name=row['peripherei'])
             new_poi = PointOfInterest(
-                       id = row['id'],
-                       latitude = row['lat'],
-                       longitude = row['lon'],
+                        id = row['id'],
+                        title = row['perigrafh'],
+                        description = row['dhmos'],
+                        latitude = row['lat'],
+                        longitude = row['lon'],
+                        category = category[0],
+                        district = row['nomarchia']
             )
             new_poi.save()
         return Response("Successfully created new PointsofInterest.",status.HTTP_201_CREATED)
