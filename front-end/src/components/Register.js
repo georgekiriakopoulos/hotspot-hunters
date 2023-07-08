0f56fa94012d5a2d569a5b502a3f2b4eac0dc6aa
Spyros@532 <sdi1900254@uoa.gr>
2023-07-08 18:56:09 +0300

    change

---
+++ front-end/src/components/Register.js
@@ -0,0 +1,59 @@
+import style from './Login.module.css';
+// export const Register = (props) => {
+//   const [email, setEmail] = useState("");
+//   const [psw, setPsw] = useState("");
+//   const [psw2, setPsw2] = useState("");
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     console.log(email);
+//   };
+
+function Register ({ open, onClose }) {
+  if (!open) return null;
+  return (
+    <div onClick={onClose} className={style.overlay}>
+      <h2> Register Form</h2>
+      <form className="registerForm" >
+        <label htmlFor="email">
+          <b>Email</b>
+        </label>
+        <input
+          type="email"
+          placeholder="π.χ youremail@youremail.com"
+          name="email"
+          required
+        />
+
+        <label htmlFor="password">
+          <b>Συνθηματικό</b>
+        </label>
+        <input
+          type="password"
+          id="password"
+          minlength="8"
+          placeholder="********"
+          name="psw"
+          required
+        />
+
+        <label htmlFor="psw2">
+          <b>Επιβεβαίωση Συνθηματικού</b>
+        </label>
+        <input
+          type="password"
+          id="password"
+          minlength="8"
+          placeholder="********"
+          name="psw2"
+          required
+        />
+
+        <button className="submit">Εγγραφή</button>
+      </form>
+      <button className="notTrue" > Είσαι ήδη μέλος; Συνδέσου!</button>
+    </div>
+  );
+};
+
+export default Register;
+
