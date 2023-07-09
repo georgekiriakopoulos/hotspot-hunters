5620e3c6b781179a12da01dcc6e408205f83bd12
konsian <konstantinougiannis98@gmail.com>
2023-07-09 12:15:28 -0700

    new database

--- backend/hotspot_api/hotspot_api/settings.py
+++ backend/hotspot_api/hotspot_api/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,15 +90,19 @@ WSGI_APPLICATION = 'hotspot_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'hotspot_db3',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hotspot_db3',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    "default": dj_database_url.parse("postgres://hotspot_postgres:NPHP5049LvyfPcR6aXOorCvyAFTHAJjM@dpg-cilfu8p5rnuvtgsdg3hg-a.frankfurt-postgres.render.com/hotspot_db3_mwob")
 }
 
 
